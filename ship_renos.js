d816f1ec3cb5ecca89f8ef85568f0164c5e9e8bf
BOZWXJ <s.mitamura@box.sakuraweb.com>
2018-05-05 12:21:46 +0900

    ship_renos.js 追加 火力，雷装，対空，装甲の、成長の余地の合計を表示する列を追加。

---
+++ ship_renos.js
@@ -0,0 +1,21 @@
+load("script/utils.js");
+ShipParameters = Java.type("logbook.dto.ShipParameters");
+
+function header() {
+	return ["改修"];
+}
+
+function begin(specdiff) { }
+
+function body(ship) {
+	var param = new ShipParameters();
+	// 成長の余地 = (装備なしのMAX) + (装備による上昇分) - (装備込の現在値)
+	param.add(ship.max);
+	param.add(ship.slotParam);
+	param.subtract(ship.param);
+	var s = param.karyoku + param.raisou + param.taiku + param.soukou;
+
+	return toComparable([ s | 0 ]);
+}
+
+function end() { }
